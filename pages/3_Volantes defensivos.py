4e83de640ee7c6364dd874debdd102ab51eecb70
robaboian <robaboian@gmail.com>
2025-05-08 22:30:32 -0300

    Update 3_Volantes defensivos.py

--- pages/3_Volantes defensivos.py
+++ pages/3_Volantes defensivos.py
@@ -11,7 +11,7 @@ def load_data():
 
 # App principal
 def main():
-    st.set_page_config(page_title="Volantes defensivos", layout="wide")
+    st.set_page_config(page_title="Volantes defensivos")
     st.subheader("Volantes defensivos")
 
     df = load_data()
