cd92fd6ec0cbba613d2da3ce202fe678eb855850
robaboian <robaboian@gmail.com>
2025-05-08 22:30:46 -0300

    Update 4_Volantes mixtos.py

--- pages/4_Volantes mixtos.py
+++ pages/4_Volantes mixtos.py
@@ -11,7 +11,7 @@ def load_data():
 
 # App principal
 def main():
-    st.set_page_config(page_title="Volantes mixtos", layout="wide")
+    st.set_page_config(page_title="Volantes mixtos")
     st.subheader("Volantes mixtos")
 
     df = load_data()
